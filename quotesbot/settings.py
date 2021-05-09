d40873fc030fd58982c5cb5464b5b5beb02f498f
Prashant Sharma <prashantsharma161198@gmail.com>
2021-05-09 21:33:10 +0530

    feat(quotesbot): scrape quotes from JS rendered site
    This scrapes quotes from https://quotes.toscrape.com/js/

---
+++ quotesbot/settings.py
@@ -0,0 +1,29 @@
+# Scrapy settings for quotesbot project
+#
+# For simplicity, this file contains only settings considered important or
+# commonly used. You can find more settings consulting the documentation:
+#
+#     https://docs.scrapy.org/en/latest/topics/settings.html
+#     https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
+#     https://docs.scrapy.org/en/latest/topics/spider-middleware.html
+
+BOT_NAME = 'quotesbot'
+
+SPIDER_MODULES = ['quotesbot.spiders']
+NEWSPIDER_MODULE = 'quotesbot.spiders'
+
+
+# Obey robots.txt rules
+ROBOTSTXT_OBEY = True
+
+# Scrapy splash settings
+SPLASH_URL = "http://localhost:8050"
+DOWNLOADER_MIDDLEWARES = {
+    'scrapy_splash.SplashCookiesMiddleware': 723,
+    'scrapy_splash.SplashMiddleware': 725,
+    'scrapy.downloadermiddlewares.httpcompression.HttpCompressionMiddleware': 810,
+}
+SPIDER_MIDDLEWARES = {
+    'scrapy_splash.SplashDeduplicateArgsMiddleware': 100,
+}
+DUPEFILTER_CLASS = 'scrapy_splash.SplashAwareDupeFilter'
